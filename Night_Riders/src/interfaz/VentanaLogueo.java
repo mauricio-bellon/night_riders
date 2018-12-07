f59af2b6dcf56f5a109ce76713c7fa0f72a04132
mauricio.bellon <mauricio.bellon@utec.edu.uy>
2018-12-07 01:46:19 -0300

    nosequé jue jue

--- Night_Riders/src/interfaz/VentanaLogueo.java
+++ Night_Riders/src/interfaz/VentanaLogueo.java
@@ -9,6 +9,7 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.JRadioButton;
 
 public class VentanaLogueo {
 
